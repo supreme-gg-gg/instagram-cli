8b8f6d2cc7b70acf18f476384626ee9e524e11b1
Jet Chiang <jetjiang.ez@gmail.com>
2025-02-21 00:04:37 -0500

    fix test case

--- tests/test.py
+++ tests/test.py
@@ -4,12 +4,14 @@ the main command runs without any errors on multiple versions of Python.
 Actual tests would be added soon as the project scales.
 """
 
-from instagram.cli import app
 import sys
 import os
-from typer.testing import CliRunner
 
+# Add the root directory (where instagram/ is located) to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from instagram.cli import app
+from typer.testing import CliRunner
 
 runner = CliRunner()
 
