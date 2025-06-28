90c99e06316c50ab0dfe3b15107bb26c03c72671
Jet Chiang <jetjiang.ez@gmail.com>
2025-06-29 00:25:50 +0800

    chore: update deps to support pastel, update docs

--- instagram-ts/test.tsx
+++ instagram-ts/test.tsx
@@ -5,13 +5,7 @@ import {render} from 'ink-testing-library';
 import App from './source/app.js';
 
 test('greet unknown user', t => {
-	const {lastFrame} = render(<App name={undefined} />);
+	const {lastFrame} = render(<App />);
 
-	t.is(lastFrame(), `Hello, ${chalk.green('Stranger')}`);
-});
-
-test('greet user with a name', t => {
-	const {lastFrame} = render(<App name="Jane" />);
-
-	t.is(lastFrame(), `Hello, ${chalk.green('Jane')}`);
+	t.not(lastFrame(), null);
 });
