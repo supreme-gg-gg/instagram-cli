ef92d43f20fab8b259f213cbde915e0412aa7347
Jet Chiang <jetjiang.ez@gmail.com>
2025-08-07 16:07:26 +0800

    refactor: ascii image display component using url

--- instagram-ts/source/ui/views/MediaView.tsx
+++ instagram-ts/source/ui/views/MediaView.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
 import {Box, Text} from 'ink';
 import {FeedItem} from '../../types/instagram.js';
-import {convertImageToColorAscii} from '../../utils/ascii-display.js';
+import AsciiImage from '../components/AsciiImage.js';
 
 type Props = {
 	feedItems: FeedItem[];
@@ -10,67 +9,55 @@ type Props = {
 };
 
 export default function MediaView({feedItems}: Props) {
-	const [asciiImages, setAsciiImages] = useState<string[]>([]);
-
-useEffect(() => {
-		const renderAscii = async () => {
-			try {
-				const images: string[] = [];
-
-				for (const item of feedItems) {
-					const url = item.image_versions2?.candidates?.[0]?.url;
-					if (url) {
-						const ascii = await convertImageToColorAscii(url);
-						console.log(ascii);
-						images.push(ascii);
-					} else {
-						images.push('No image');
-					}
-				}
-				setAsciiImages(images);
-			} catch (err) {
-				console.error('Error converting images to ASCII:', err);
-			} finally {
-			}
-		};
-
-		renderAscii();
-	}, [feedItems]);
-
 	return (
 		<Box flexDirection="column" gap={1}>
 			<Text color="blue">Your Feed</Text>
-			{feedItems.map((item, index) => (
-				<Box
-					key={item.id}
-					flexDirection="column"
-					borderStyle="round"
-					padding={1}
-				>
-					<Box flexDirection="row">
-						<Text color="green">👤 {item.user?.username || 'Unknown user'}</Text>
-						<Text color="gray">{' ('}{new Date(item.taken_at * 1000).toLocaleString()}{')'}</Text>
-					</Box>
-					<Text>{'\n'}</Text>
-					<Text>{item.caption?.text || 'No caption'}</Text>
-					<Text>{'\n'}</Text>
+			{feedItems.map(item => {
+				const url = item.image_versions2?.candidates?.[0]?.url;
 
-					<Box flexDirection="column">
-						{/* TODO: Handling properly posts with multiple images */}
-						{asciiImages[index] ? (
-							asciiImages[index]
-							.split('\n')
-							.map((line, i) => <Text key={i}>{line}</Text>)
-						) : (
-							<Text color="yellow">⏳ Loading media...</Text>
-						)}
+				return (
+					<Box
+						key={item.id}
+						flexDirection="column"
+						borderStyle="round"
+						padding={1}
+					>
+						<Box flexDirection="row">
+							<Text color="green">
+								👤 {item.user?.username || 'Unknown user'}
+							</Text>
+							<Text color="gray">
+								{' ('}
+								{new Date(item.taken_at * 1000).toLocaleString()}
+								{')'}
+							</Text>
+						</Box>
+						<Text>{'\n'}</Text>
+						<Text>{item.caption?.text || 'No caption'}</Text>
+						<Text>{'\n'}</Text>
+
+						<Box flexDirection="column">
+							{/* TODO: Handling properly posts with multiple images */}
+							{url ? (
+								<AsciiImage url={url} />
+							) : (
+								<Text color="yellow">⏳ No media found...</Text>
+							)}
+						</Box>
+						<Box flexDirection="row">
+							<Text>
+								{' '}
+								♡ {item.like_count ?? 0}
+								{'   '}
+							</Text>
+							<Text>
+								🗨{'  '}
+								{item.comment_count ?? 0}
+							</Text>
+						</Box>
 					</Box>
-					<Box flexDirection="row">
-						<Text>{' '}♡ {item.like_count ?? 0}{"   "}</Text>
-						<Text>🗨{"  "}{item.comment_count ?? 0}</Text>
-					</Box>
-				</Box>
-			))}
+				);
+			})}
 		</Box>
 	);
 }
