2019b9ad50fbb47b224f55d1368cb0360ee4ee9f
James Zheng <endernoke@gmail.com>
2025-09-19 13:41:28 +0800

    feat: make logo colorful

--- instagram-ts/source/commands/index.tsx
+++ instagram-ts/source/commands/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import Gradient from 'ink-gradient';
 import BigText from 'ink-big-text';
 import {Text} from 'ink';
 
 export default function Index() {
 	return (
 		<>
-			<BigText text="InstagramCLI" font="simple" colors={['#ff00ff']} />
+			<Gradient
+				colors={[
+					'#405DE6',
+					'#5B51D8',
+					'#833AB4',
+					'#C13584',
+					'#E1306C',
+					'#FD1D1D',
+					'#F56040',
+				]}
+			>
+				<BigText text="Instagram CLI" colors={['#ff00ff']} />
+			</Gradient>
 			<Text color="green">The end of brainrot and doomscrolling is here.</Text>
 			<Text color="blue">
 				Type &#39;instagram --help&#39; to see available commands.
